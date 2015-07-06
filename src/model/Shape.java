a6150bdabaf86e83bb2fbd56a00b697f527701d3
Dennis Anderson <Dennis.Anderson@sogeti.nl>
2015-07-06 11:01:52 +0200

    first init

---
+++ src/model/Shape.java
@@ -0,0 +1,8 @@
+package model;
+
+public abstract class Shape {
+
+	public void draw(){
+		System.out.println("Inside Shape::draw() method.");
+	}
+}
---
+++ src/model/Square.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Square extends Shape {
+
+// 	When this method is not in Square class it will use the method from the super class. So it will print Shape. 
+//	@Override
+//	public void draw(){
+//		System.out.println("Inside Square::draw() method.");
+//	}
+	
+}

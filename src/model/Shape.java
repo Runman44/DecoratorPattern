f7c376928b53987a1a725fcb133692d5c4669d4d
Dennis Anderson <Dennis.Anderson@sogeti.nl>
2015-07-06 11:37:32 +0200

    adding comments

--- src/model/Shape.java
+++ src/model/Shape.java
@@ -2,6 +2,7 @@ package model;
 
 public abstract class Shape {
 
+	// Making this method abstract will remove the implementation of the draw method in this abstract class.
 	public void draw(){
 		System.out.println("Inside Shape::draw() method.");
 	}
